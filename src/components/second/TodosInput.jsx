9ed5e86136ff22fe8ba5beea575ea5d9baa3c191
jaba777 <tskhovreba777@gmail.com>
2022-10-05 23:31:33 +0400

    add functions

--- src/components/second/TodosInput.jsx
+++ src/components/second/TodosInput.jsx
@@ -3,19 +3,51 @@ import './TodosInput.css';
 
 const TodosInput = () => {
 
-    const [inputText,setInputText]=useState('');
+    const [inputText,setInputText]=useState(null);
     const [inputCont,setInputCont] = useState([]);
+
+    const inputChangeHandler=(event)=>{
+        setInputText(event.target.value);
+    }
+
+    const submitHandler=(event)=>{
+        event.preventDefault();
+
+        if(inputText.trim() === ''){
+            return;
+        }
+
+        const mass={
+            id: Math.random(),
+            text: inputText,
+        }
+        setInputText('');
+        setInputCont([...inputCont,mass]);
+    }
+
+    const removeHandler=(item)=>{
+        setInputCont(inputCont.filter(event => event.id !== item.id));
+    }
 
   return (
     <section className='todoInput'>
         <h1>todos</h1>
 
         <div className='submit-container'>
-            <form>
-                <input type="text" className='input' />
+            <form onSubmit={submitHandler}>
+                <input type="text" className='input' onChange={inputChangeHandler} value={inputText} />
             </form>
 
-            
+            <ul className='inputs-text'>
+                {inputCont.map((item,ind) => {
+                    return(
+                        <li key={ind}>
+                            <p>{item.text}</p>
+                            <button className='delete' onClick={()=> removeHandler(item)}><i class="fa-solid fa-x"></i></button>
+                        </li>
+                    )
+                })}
+            </ul>
             
         </div>
     </section>
